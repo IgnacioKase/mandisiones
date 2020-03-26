d8da9375a8364ab4d84b408718656661b2a4eb6f
Ignacio Kasevich <ignacio@kasevich.com.ar>
2020-03-26 15:32:55 -0300

    Template

--- missions/test_template.tem_anizay/arga_settings.hpp
+++ missions/test_template.tem_anizay/arga_settings.hpp
@@ -2,30 +2,36 @@
                           Realizado por |ArgA|Ignacio
 *******************************************************************************/
 
+#define NAME "Op. "
+#define IMAGE "imgs\"
+#define DESCRIPTION ""
+
 author = "";                            // Editor
-onLoadName = "";	                    // Nombre de carga
-briefingName = "";                      // Nombre en briefing
-onLoadMission = "";                     // Pie de carga
-overviewText = "";                      // Pie en seleccion de mision
-overviewPicture = "imgs\portada.jpg";   // Imagen en seleccion de mision
-loadScreen = "imgs\portada.jpg";        // Imagen en carga
+onLoadName = NAME;	                    // Nombre de carga
+briefingName = NAME;                    // Nombre en briefing
+onLoadMission = DESCRIPTION;            // Pie de carga
+overviewText = DESCRIPTION;             // Pie en seleccion de mision
+overviewPicture = IMAGE;                // Imagen en seleccion de mision
+loadScreen = IMAGE;                     // Imagen en carga
 IMG_LOGO = "imgs\logo.paa"              // Nota: Las imagenes deberian estar en
 COLOR_AUTOR = "#76a8f7";                // formato .paa y en proporcion 2x1 (1024x512)
 ES_CAMPANIA = 0;                        // tamaño logo: 323px x 323px
 NOMBRE_CAMPANIA = "";
 
-MAX_DIST_VISION = 2500;
+MAX_DIST_VISION = 3000;
 GAFAS_INICIALES = "";
 DESACTIVAR_EQUIPAMIENTO_PERSONALIZADO = 1;
 
 //BRIEFING
+// Cada elemento del array es un parrafo
+// Ejemplo: ["Primer parrafo", "Segundo parrafo"]
 CARACTER = "No Oficial";
 HORARIO = "";
 FECHA = "";
 CLIMA = "";
 UBICACION = "";
-SITUACION = [""];               //Cada elemento del array es un parrafo
-SITUACION_HISTORICA = [""];     //Ejemplo: ["Primer parrafo", "Segundo parrafo"]
+SITUACION = [""];               
+SITUACION_HISTORICA = [""];     
 EXTRACTO_INTELIGENCIA = [""];
 OBJETIVOS = ["", ""];
 RECURSOS = ["", ""];
@@ -34,7 +40,8 @@ ENEMIGOS = ["", ""];
 
 //IA - CONFIGURACION
 DESACTIVAR_IA_DE_GRUPO = 1;
-DIFICULTAD_IA_PERSONALIZADA = 1;
+DESACTIVAR_TODO_BLUFOR = 1;
+DIFICULTAD_IA_PERSONALIZADA = 0;
 AIMING_ACCURACY = 0.01; //precision de apuntado
 AIMING_SHAKE = 0.99; 	//dispersión de apuntado
 AIMING_SPEED = 0.1; 	//velocidad de apuntado
