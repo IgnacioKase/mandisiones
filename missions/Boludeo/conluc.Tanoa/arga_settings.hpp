e6a0362306ed6a511a8737ab234f512519a7b46d
Maxi <maxitreggi@gmail.com>
2020-04-16 20:46:10 -0300

    competencia arga 2020

---
+++ missions/Boludeo/conluc.Tanoa/arga_settings.hpp
@@ -0,0 +1,57 @@
+/*******************************************************************************
+                          Realizado por |ArgA|Ignacio
+*******************************************************************************/
+
+#define NAME "Op. ФСБ Альфа v6"
+#define IMAGE "imgs\portada.paa"
+#define DESCRIPTION "Matar yankees"
+
+author = "Maxi e Ignacio";                       
+onLoadName = NAME;	                    
+briefingName = NAME;                      
+onLoadMission = DESCRIPTION;                     
+overviewText = DESCRIPTION;                      
+overviewPicture = IMAGE;   
+loadScreen = IMAGE;        
+IMG_LOGO = "imgs\logo.paa"              
+COLOR_AUTOR = "#44CC00";               
+ES_CAMPANIA = 0;                        
+NOMBRE_CAMPANIA = "NO ES UNA CAMPAÑA";
+
+MAX_DIST_VISION = 2600;
+GAFAS_INICIALES = "arga_rhs_pm_negro";
+DESACTIVAR_EQUIPAMIENTO_PERSONALIZADO = 1;
+
+//BRIEFING
+// Cada elemento del array es un parrafo
+// Ejemplo: ["Primer parrafo", "Segundo parrafo"]
+CARACTER = "No Oficial";
+HORARIO = "1645 hrs";
+FECHA = "08/04/2020";
+CLIMA = "Nevando/ Neblina alta";
+UBICACION = "Chernarus Winter";
+SITUACION = ["Hace tiempo se desestabilizo la situación en Chernarus, eso llevo a revueltas y conflictos armados. Luego de unos meses EEUU decidió mandar tropas para ayudar. Esta noticia no fue muy bien recibida en Rusia debido a que esta era una zona protegida por Rusia con bases, tanto operativas como inoperativas, que ahora se ven comprometidas. Rusia decidió reforzar la presencia militar para injustificar la presencia yankee en la zona.", "Hace escasos minutos recibimos del equipo Spetnaz Вымпел (Výmpel) fue embozcado en un antiguo FOB en la localidad de Msta, se cree que el ataque fue realizado por EEUU pero no esta confirmado. En este momento el equipo Spetnaz СМЕРШ (Smersh) y ФСБ Альфа (Grupo Alfa) se dirigen a Msta para realizar apoyo.", "Este ataque indiscriminado no puede quedar así por lo que al reagrupar realizaremos un ataque a Elektrozavodsk para equilibrar la balanza."];               
+SITUACION_HISTORICA = [""];     
+EXTRACTO_INTELIGENCIA = [""];
+OBJETIVOS = ["Ayudar a Вымпел (Výmpel)", "Equilibrar la balanza"];
+RECURSOS = ["Aparecemos Armados", "4 GAZ-233014 (Artillado)", "2 GAZ-233011", "2 GAZ-233011 (Camo)(Médico)"];
+ENEMIGOS = ["Fuerzas enemigas bien entrenadas", "Vehículos ligeros", "Vehículos Pesados"];
+////////////////////////////////////////////////////////////////////////////////
+
+//IA - CONFIGURACION
+DESACTIVAR_IA_DE_GRUPO = 1;
+DESACTIVAR_TODO_BLUFOR = 0;
+DIFICULTAD_IA_PERSONALIZADA = 0;
+AIMING_ACCURACY = 0.01;
+AIMING_SHAKE = 0.99; 	
+AIMING_SPEED = 0.1; 	
+SPOT_DISTANCE = 0.2; 	
+SPOT_TIME = 0.5; 		
+COURAGE = 0.25; 		
+RELOAD_SPEED = 0.25; 	
+COMMANDING = 0.75; 		
+////////////////////////////////////////////////////////////////////////////////
+
+/*******************************************************************************
+                          Realizado por |ArgA|Ignacio
+*******************************************************************************/
