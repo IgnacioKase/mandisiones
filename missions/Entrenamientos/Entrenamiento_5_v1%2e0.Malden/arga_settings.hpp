21b1a7451d8b7154313a717520fc95a3a4ea9363
Ignacio Kasevich <ignacio@kasevich.com.ar>
2020-04-27 20:48:43 -0300

    Entrenamiento

---
+++ missions/Entrenamientos/Entrenamiento_5_v1%2e0.Malden/arga_settings.hpp
@@ -0,0 +1,57 @@
+/*******************************************************************************
+                          Realizado por |ArgA|Ignacio
+*******************************************************************************/
+
+#define NAME "Mapa de Entrenamiento ArgA"                     
+#define IMAGE "imgs\"                  
+#define DESCRIPTION "Escuela de infanteria Oscar Poltronieri"
+
+author = "Maxi e Ignacio";                            
+onLoadName = NAME;	                   
+briefingName = NAME;                   
+onLoadMission = DESCRIPTION;           
+overviewText = DESCRIPTION;             
+overviewPicture = IMAGE;                
+loadScreen = IMAGE;                     
+IMG_LOGO = "imgs\logo.paa"             
+COLOR_AUTOR = "#76a8f7";                
+ES_CAMPANIA = 0;                        
+NOMBRE_CAMPANIA = "";
+INTRO = 3                               
+MAX_DIST_VISION = 4000;
+GAFAS_INICIALES = "";
+DESACTIVAR_EQUIPAMIENTO_PERSONALIZADO = 1;
+
+//BRIEFING
+// Cada elemento del array es un parrafo
+// Ejemplo: ["Primer parrafo", "Segundo parrafo"]
+CARACTER = "No Oficial";
+HORARIO = "";
+FECHA = "";
+CLIMA = "";
+UBICACION = "";
+SITUACION = [""];               
+SITUACION_HISTORICA = [""];     
+EXTRACTO_INTELIGENCIA = [""];
+OBJETIVOS = ["", ""];
+RECURSOS = ["", ""];
+ENEMIGOS = ["", ""];
+////////////////////////////////////////////////////////////////////////////////
+
+//IA - CONFIGURACION
+DESACTIVAR_IA_DE_GRUPO = 1; // 0 Permite la IA del Grupo de los jugadores, 1 la desactiva.
+DESACTIVAR_TODO_BLUFOR = 0; // 0 Permite la IA del Blufor, 1 la desactiva.
+DIFICULTAD_IA_PERSONALIZADA = 0;
+AIMING_ACCURACY = 0.01; //precision de apuntado
+AIMING_SHAKE = 0.99; 	//dispersión de apuntado
+AIMING_SPEED = 0.1; 	//velocidad de apuntado
+SPOT_DISTANCE = 0.3; 	//distancia de avistamiento
+SPOT_TIME = 0.5; 		//tiempo de avistamiento
+COURAGE = 0.25; 		//coraje
+RELOAD_SPEED = 0.25; 	//velocidad de recarga
+COMMANDING = 0.75; 		//liderazgo
+////////////////////////////////////////////////////////////////////////////////
+
+/*******************************************************************************
+                          Realizado por |ArgA|Ignacio
+*******************************************************************************/
