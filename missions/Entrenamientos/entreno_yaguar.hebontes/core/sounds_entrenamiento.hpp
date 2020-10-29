80ce9a4b41611054f8be72180a45d90167cdca9a
klosther <56041231+klosther@users.noreply.github.com>
2020-10-29 07:35:41 -0300

    mision ani

---
+++ missions/Entrenamientos/entreno_yaguar.hebontes/core/sounds_entrenamiento.hpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+                          Realizado por |ArgA|MIV
+*******************************************************************************/
+
+#include "sounds.hpp"
+
+class MANDI_TRUMPET
+{
+	name = "MANDI_TRUMPET";
+	sound[] = {"core\sounds\Track_trumpet.ogg", 1, 1};
+	titles[] = {};
+};
+
+class MANDI_ANTHEM
+{
+	name = "MANDI_ANTHEM";
+	sound[] = {"core\sounds\Track_anthem.ogg", 1, 1};
+	titles[] = {};
+};
+
+class MANDI_BUS
+{
+	name = "MANDI_BUS";
+	sound[] = {"core\sounds\Tack_bus.ogg", 1, 1};
+	titles[] = {};
+};
+
+/*******************************************************************************
+                          Realizado por |ArgA|MIV
+*******************************************************************************/
